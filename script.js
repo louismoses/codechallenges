8e5aaf351096baa4146098a0a89b55c61e227902
Louis Moses <louismoses.rait@gmail.com>
2023-01-26 17:51:39 +0800

    Highest Score display

--- script.js
+++ script.js
@@ -21,6 +21,9 @@ printForecast(testData2);
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 
+let highScore = 0;
+
+//check my guess --------------------------
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   // score not zero
@@ -36,6 +39,11 @@ document.querySelector(".check").addEventListener("click", function () {
       // inline css
       document.querySelector("body").style.backgroundColor = "#60b347";
       document.querySelector(".number").style.width = "30rem";
+      // high score
+      if (score > highScore) {
+        highScore = score;
+        document.querySelector(".highscore").textContent = highScore;
+      }
     }
     //number too high
     else if (guess > secretNumber) {
@@ -53,8 +61,8 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector(".message").textContent = "💥You Lost the Game ";
   }
 });
+//play again -------------------
 document.querySelector(".again").addEventListener("click", function () {
-  console.log("again", secretNumber);
   //restore score
   score = 20;
   //restore secret number
@@ -69,4 +77,3 @@ document.querySelector(".again").addEventListener("click", function () {
   document.querySelector("body").style.backgroundColor = "#222";
   document.querySelector(".number").style.width = "15rem";
 });
-console.log(secretNumber);
