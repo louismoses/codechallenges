acd14a476044645bbc21c81d554217e89fd07df8
Louis Moses <louismoses.rait@gmail.com>
2023-02-01 19:59:34 +0800

    -update: dice starting condition implemented

--- script.js
+++ script.js
@@ -138,4 +138,15 @@ if (modalPage) {
 //checks if this script is needed on the page
 const pigDice = document.querySelector("#pig-dice-game");
 if (pigDice) {
+  //selecting elements
+  const score0El = document.querySelector("#score--0");
+  const score1El = document.querySelector("#score--1");
+  const diceEl = document.querySelector(".dice");
+
+  //starting conditions
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  diceEl.classList.add("hidden");
+
+  const newGame = function () {};
 }
