0554995b437b7ea5471a1a1b38e5b52477ea80e1
Louis Moses <louismoses.rait@gmail.com>
2023-02-06 11:08:32 +0800

    -update : switch player and reset current score

--- script.js
+++ script.js
@@ -139,6 +139,8 @@ if (modalPage) {
 const pigDice = document.querySelector("#pig-dice-game");
 if (pigDice) {
   //selecting elements
+  const player0El = document.querySelector(".player--0");
+  const player1El = document.querySelector(".player--1");
   const score0El = document.querySelector("#score--0");
   const score1El = document.querySelector("#score--1");
   const current0El = document.getElementById("current--0");
@@ -153,6 +155,7 @@ if (pigDice) {
   diceEl.classList.add("hidden");
 
   let currentScore = 0;
+  let activePlayer = 0;
 
   const rollDice = function () {
     //generate random number
@@ -162,10 +165,18 @@ if (pigDice) {
     diceEl.src = `dice-${diceValue}.png`;
     //
     if (diceValue === 1) {
+      // switch to next player
+      document.getElementById(`current--${activePlayer}`).textContent = 0;
+      activePlayer = activePlayer === 0 ? 1 : 0;
+      currentScore = 0;
+      player0El.classList.toggle("player--active");
+      player1El.classList.toggle("player--active");
+
       console.log("reset score");
     } else {
       currentScore += diceValue;
-      current0El.textContent = currentScore;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
       console.log("add score to current score", currentScore);
     }
   };
