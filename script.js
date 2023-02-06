676c03c3e213de29294566ed421ec036879c3e73
Louis Moses <louismoses.rait@gmail.com>
2023-02-06 13:34:27 +0800

    -update : finished the pig-dice-game

--- script.js
+++ script.js
@@ -149,16 +149,31 @@ if (pigDice) {
   const diceEl = document.querySelector(".dice");
   const btnRoll = document.querySelector(".btn--roll");
   const btnHold = document.querySelector(".btn--hold");
+  const btnNew = document.querySelector(".btn--new");
 
-  //starting conditions
-  score0El.textContent = 0;
-  score1El.textContent = 0;
-  diceEl.classList.add("hidden");
+  let scores, currentScore, activePlayer, playing;
 
-  const scores = [0, 0];
-  let currentScore = 0;
-  let activePlayer = 0;
-  let playing = true;
+  const init = function () {
+    scores = [0, 0];
+    currentScore = 0;
+    activePlayer = 0;
+    playing = true;
+
+    //starting conditions
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+
+    diceEl.classList.add("hidden");
+
+    player0El.classList.remove("player--winner");
+    player0El.classList.add("player--active");
+    player1El.classList.remove("player--winner");
+    player1El.classList.remove("player--active");
+  };
+
+  init();
 
   const switchPlayer = function () {
     document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -210,6 +225,7 @@ if (pigDice) {
       }
     }
   });
+  btnNew.addEventListener("click", init);
 }
 
 //video 85
